e7d3d4b10a5701c7f7c2d5adfaf78044f526f166
chuan137 <chuan137@gmail.com>
2023-05-31 23:10:06 +0200

    update

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,3 +1,5 @@
+local U = require("user.util")
+
 return {
 
   {
@@ -5,6 +7,9 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
+      "ray-x/lsp_signature.nvim",
+      "SmiteshP/nvim-navic",
+      "SmiteshP/nvim-navbuddy",
     },
     init = function()
       local ui_opts = { border = "rounded" }
@@ -17,37 +22,26 @@ return {
       require("vim.lsp.log").set_format_func(vim.inspect)
 
       local nvim_lsp = require("lspconfig")
+      local lspsignature_config = {
+        bind = true,
+        toggle_key = "<C-k>",
+      }
 
-      local on_attach = function(_, bufnr)
+      local on_attach = function(client, bufnr)
+        require("lsp_signature").on_attach(lspsignature_config, bufnr)
+        require("nvim-navic").attach(client, bufnr)
+        require("nvim-navbuddy").attach(client, bufnr)
+
+        -- buffer local options
         vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-        -- Mappings.
-        -- stylua: ignore start
-        local kopts = { buffer = bufnr, noremap = true, silent = true }
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, kopts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, kopts)
-        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, kopts)
-        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, kopts)
-        vim.keymap.set("n", "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, kopts)
-        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, kopts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, kopts)
-        vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, kopts)
-
-        vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, kopts)
-        vim.keymap.set({ "n", "x" }, "<F3>", function() vim.lsp.buf.format({ async = true }) end, kopts)
-        vim.keymap.set({ "n", "x" }, "<F4>", vim.lsp.buf.code_action, kopts)
-
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, kopts)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, kopts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, kopts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, kopts)
-        vim.keymap.set("n", "gl", vim.diagnostic.open_float, kopts)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, kopts)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, kopts)
+        -- mappings
+        U.import("keymaps").setup(bufnr)
       end
 
-      local servers = require("plugins.lsp.servers")
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- local servers = require("plugins.lsp.servers")
+      local servers = U.import("servers") or {}
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       for name, server in pairs(servers) do
         nvim_lsp[name].setup({
@@ -79,5 +73,35 @@ return {
         },
       }
     end,
+  },
+
+  -- navbuddy
+  {
+    "SmiteshP/nvim-navbuddy",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      window = {
+        border = "rounded",
+        size = { height = "50%", width = "70%" },
+        position = { row = "100%", col = "100%" },
+        sections = {
+          left = {
+            size = "25%",
+          },
+          mid = {
+            size = "50%",
+          },
+          right = {
+            preview = "never",
+          },
+        },
+      },
+    },
+    -- config = function(_, opts)
+    --   require("nvim-navbuddy").setup(opts)
+    -- end,
   },
 }
---
+++ lua/plugins/lsp/keymaps.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+function M.setup(bufnr)
+  -- Mappings.
+  -- stylua: ignore start
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+  -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+  vim.keymap.set("n", "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+
+  vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+  vim.keymap.set({ "n", "x" }, "<F3>", function() vim.lsp.buf.format({ async = true }) end, opts)
+  vim.keymap.set({ "n", "x" }, "<F4>", vim.lsp.buf.code_action, opts)
+
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+  vim.keymap.set("n", "<leader>go", function() require("nvim-navbuddy").open() end, opts)
+end
+
+return M
