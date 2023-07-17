3b5374d3c6a72ec04301fb9d17bb5b81b1178c12
chuan137 <chuan137@gmail.com>
2023-07-17 10:31:15 +0200

    update lsp keymaps

--- lua/plugins/lsp/keymaps.lua
+++ lua/plugins/lsp/keymaps.lua
@@ -4,13 +4,11 @@ function M.setup(bufnr)
   -- Mappings.
   -- stylua: ignore start
   local opts = { buffer = bufnr, noremap = true, silent = true }
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-  -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
   vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
   vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
   vim.keymap.set("n", "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
   vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
   vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 
   vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
@@ -20,12 +18,14 @@ function M.setup(bufnr)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
   vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+  -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-
-  vim.keymap.set("n", "<leader>go", function() require("nvim-navbuddy").open() end, opts)
 end
 
 return M
