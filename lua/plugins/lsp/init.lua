3a7f282dc09db31940a24cd53f561cff575dfb6c
chuan137 <chuan137@gmail.com>
2023-10-07 17:29:19 +0200

    lsp: disable lsp signature

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -7,47 +7,48 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
-      "ray-x/lsp_signature.nvim",
+      -- "ray-x/lsp_signature.nvim",
     },
     config = function()
       -- Lsp logging
       vim.lsp.set_log_level("info")
       require("vim.lsp.log").set_format_func(vim.inspect)
 
-      local nvim_lsp = require("lspconfig")
-      local lspsignature_config = {
-        bind = true,
-        toggle_key = "<C-k>",
-      }
-
-      local on_attach = function(client, bufnr)
-        require("lsp_signature").on_attach(lspsignature_config, bufnr)
-
-        -- buffer local options
-        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-        -- mappings
-        U.import("keymaps").setup(bufnr)
-      end
-
-      -- local servers = require("plugins.lsp.servers")
-      local servers = U.import("servers") or {}
       -- ui
       local ui_opts = { border = "rounded" }
       vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, ui_opts)
       vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, ui_opts)
 
+      -- load lsp server configs and keymaps from ./servers.lua and ./keymaps.lua
+      local servers = require("plugins.lsp.servers") or {}
+      local keymaps = require("plugins.lsp.keymaps") or {}
+
+      -- callback on LSP attach
+      -- check ":help lsp" for more info
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(ev)
+          keymaps.setup(ev.buf)
+        end,
+      })
+
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       for name, server in pairs(servers) do
-        nvim_lsp[name].setup({
+        require("lspconfig")[name].setup({
           capabilities = capabilities,
-          cmd = server["cmd"],
-          on_attach = on_attach,
           settings = server["settings"],
         })
       end
     end,
-  }
+  },
+
+  -- {
+  --   "lsp_signature.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     -- hint_enable = false,
+  --     toggle_key = "<C-k>",
+  --   },
+  -- }
 
 }
